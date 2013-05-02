e68f535bd1690513a0dab8b7fe9ee87b593cd546
Anish Tondwalkar <2012atondwal@tjhsst.edu>
2013-05-01 21:32:52 -0400

    inital commit

---
+++ xmonad.hs
@@ -0,0 +1,26 @@
+import XMonad
+import XMonad.Config.Gnome
+import XMonad.Actions.MouseGestures
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+
+main = xmonad $ defaultConfig {
+    terminal	= "urxvtc"
+  , modMask	= mod4Mask
+  , workspaces	= map show ['1'..'9']
+}
+
+myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
+    [ ((modm, button1), mouseGesture gestures)
+    , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
+    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
+                                       >> windows W.shiftMaster))
+    , ((modm, button4), (\w -> focus w >> windows W.swapUp))
+    , ((modm, button5), (\w -> focus w >> windows W.swapDown))
+    ]
+
+gestures = M.fromList $
+    [ ([], (\w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster))
+    , ([L],(\w -> focus w >> screenWorkspace 0 >>= flip whenJust (windows . W.shift)))
+    , ([R],(\w -> focus w >> screenWorkspace 1 >>= flip whenJust (windows . W.shift)))
+    ]
